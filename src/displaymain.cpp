07eda698912d584b5ff108ba1e4b82721ac74bd6
lou000 <44552514+lou000@users.noreply.github.com>
2020-11-25 03:46:50 +0100

    implement displayMain functionality

--- src/displaymain.cpp
+++ src/displaymain.cpp
@@ -2,8 +2,11 @@
 
 DisplayMain::DisplayMain(QWidget *parent) : QWidget(parent)
 {
-    displayText = new QLabel("0", this);
-    displayText->setAlignment(Qt::AlignRight);
+    displayText = new QLabel("0", this); //startup values set here and in class declaration
+
+    displayText->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+    displayText->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    displayText->setWordWrap(true);
     auto layout = new QGridLayout(this);
     layout->addWidget(displayText);
     this->setLayout(layout);
@@ -19,9 +22,115 @@ DisplayMain::DisplayMain(QWidget *parent) : QWidget(parent)
     displayText->setFont(font);
 }
 
+void DisplayMain::setNumeralSystem(Calc::NumeralSystem sys)
+{
+    dispSystem = sys;
+    switch (dispSystem) {
+
+    case Calc::NumeralSystem::Oct:
+    case Calc::NumeralSystem::Dec:
+    case Calc::NumeralSystem::Hex:
+    {
+        QFont font;
+        font.setPixelSize(50);
+        displayText->setFont(font);
+        break;
+    }
+
+    case Calc::NumeralSystem::Bin:
+    {
+        QFont font;
+        font.setPixelSize(22);
+        displayText->setFont(font);
+        break;
+    }
+    }
+    displayNumber(number);
+}
+
+void DisplayMain::toggleGroupNumbers()
+{
+    grouped = !grouped;
+    displayNumber(number);
+}
+
+void DisplayMain::copyFromDisplay()
+{
+    QApplication::clipboard()->setText(displayText->text());
+}
+
 // This is the only function that we need right now
 // later i might add a second label to display memory, last number and currently executed function
-void DisplayMain::displayNumber(qint64 number, Calc::NumeralSystem dispSystem)
+void DisplayMain::displayNumber(qint64 num)
 {
-    displayText->setText(QString::number(number));
+    number = num;
+    switch (dispSystem) {
+    case Calc::NumeralSystem::Bin:
+    {
+        auto text = QString::fromStdString(std::bitset<64>(num).to_string());
+        //remove leading zeros
+        for(int i=0; i<text.length(); i++)
+        {
+            if(text.at(i) == "0")
+            {
+                text.remove(i, 1);
+                i--;
+            }
+            else break;
+        }
+        if(grouped)
+        {
+            for(int i=text.length()-4; i>=0; i-=4)
+            {
+                text.insert(i, "   ");
+            }
+            if(text.length() > 32 + 7*3)    // 7*4 - space added in prev step
+                text.insert(text.length()-32 - 7*3, "\n");
+            displayText->setText(text);
+        }
+        else{
+            if(text.length() > 32)
+                text.insert(text.length()-32, "\n");
+            displayText->setText(text);
+        }
+        break;
+    }
+
+    case Calc::NumeralSystem::Oct:
+    {
+        auto text = QString::number(num, 8);
+        if(grouped)
+            for(int i=text.length()-3; i>=0; i-=3)
+            {
+                text.insert(i, " ");
+            }
+        displayText->setText(text);
+        break;
+    }
+
+    case Calc::NumeralSystem::Dec:
+    {
+        auto text = QString::number(num);
+        if(grouped)
+            for(int i=text.length()-3; i>=0; i-=3)
+            {
+                text.insert(i, " ");
+            }
+        displayText->setText(text);
+        break;
+    }
+
+    case Calc::NumeralSystem::Hex:
+    {
+        auto text = QString::number(num, 16).toUpper();
+        if(grouped)
+            for(int i=text.length()-4; i>=0; i-=4)
+            {
+                text.insert(i, " ");
+            }
+        displayText->setText(text);
+        break;
+    }
+    }
 }
+
--- src/displaymain.h
+++ src/displaymain.h
@@ -6,6 +6,9 @@
 #include <QGridLayout>
 #include <QStyleOption>
 #include <QPainter>
+#include <QApplication>
+#include <QClipboard>
+#include <bitset>
 #include "globalinclude.h"
 
 class DisplayMain : public QWidget
@@ -17,7 +20,10 @@ public:
     explicit DisplayMain(QWidget *parent = nullptr);
 
 private:
+    qint64 number = 0;
+    bool grouped = false;
     QLabel* displayText;
+    Calc::NumeralSystem dispSystem = Calc::NumeralSystem::Dec;
     void paintEvent(QPaintEvent *) {
             QStyleOption opt;
             opt.init(this);
@@ -26,7 +32,13 @@ private:
     }
 
 public slots:
-    void displayNumber(qint64 number, Calc::NumeralSystem dispSystem);
+    void setNumeralSystem(Calc::NumeralSystem sys);
+    void displayNumber(qint64 number);
+    void toggleGroupNumbers();
+    // We can do the copy here but paste should go through controller
+    // so it can be properly distribiuted to both displays and also
+    // modified to appropriate precision
+    void copyFromDisplay();
 
 };
 
